63e76c5791d022a01e7705cca8a8cb9f73579e91
isa <5445946+icortes@users.noreply.github.com>
2023-03-09 22:22:15 -0800

    using nanoID for url shortening generator

--- pages/api/urlCreate.ts
+++ pages/api/urlCreate.ts
@@ -1,17 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { nanoid } from 'nanoid';
 import { supabase } from '@/lib/supabaseClient';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<string>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method === 'POST') {
       let { body } = req;
       let original_url = body.original_url as string;
-      console.log(req.method);
-      console.log(original_url);
-      res.status(200).send(original_url);
+
+      let short_url = `${req.headers.host}/${nanoid(6)}`;
+
+      res.status(200).send({ original_url, short_url });
     }
   } catch (err) {
     res.status(500).send('failed to load data');
